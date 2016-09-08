de1580a31c8245b50dc103532352e33dd41306c5
saikikwok <saikikwok91@gmail.com>
2016-09-08 09:13:34 -0700

    add task details layout

--- app/src/main/java/com/example/saikikwok/todolist/Models/Todo.java
+++ app/src/main/java/com/example/saikikwok/todolist/Models/Todo.java
@@ -1,16 +1,67 @@
 package com.example.saikikwok.todolist.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by saikikwok on 8/30/16.
  */
 
-public class Todo {
+public class Todo implements Parcelable {
+
+    private String id;
     private String task;
+    private boolean done;
     private Date invokedDate;
 
     public Todo() {}
+
+    public Todo(String task, Date invokedDate) {
+        this.id = UUID.randomUUID().toString();
+        this.task = task;
+        this.done = false;
+        this.invokedDate = invokedDate;
+    }
+
+    public static final Creator<Todo> CREATOR = new Creator<Todo>() {
+        @Override
+        public Todo createFromParcel(Parcel in) {
+            return new Todo(in);
+        }
+
+        @Override
+        public Todo[] newArray(int size) {
+            return new Todo[size];
+        }
+    };
+
+    private Todo(Parcel in) {
+        this.id = in.readString();
+        this.task = in.readString();
+        this.done = in.readByte() != 0;
+        long date = in.readLong();
+        this.invokedDate = date == 0 ? null : new Date(date);
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
 
     public String getTask() {
         return task;
@@ -26,5 +77,18 @@ public class Todo {
 
     public void setInvokedDate(Date invokedDate) {
         this.invokedDate = invokedDate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(task);
+        parcel.writeByte((byte) (done ? 1 : 0));
+        parcel.writeLong(invokedDate == null ? 0 : invokedDate.getTime());
     }
 }
---
+++ app/src/main/java/com/example/saikikwok/todolist/TaskActivity.java
@@ -0,0 +1,18 @@
+package com.example.saikikwok.todolist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by saikikwok on 8/30/16.
+ */
+
+public class TaskActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
