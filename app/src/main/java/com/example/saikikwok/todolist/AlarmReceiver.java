d7202d881af4309219832bdad89d864e7f38c178
saikikwok <saikikwok91@gmail.com>
2016-09-16 09:30:14 -0700

    fixing bugs on store via notification

--- app/src/main/java/com/example/saikikwok/todolist/AlarmReceiver.java
+++ app/src/main/java/com/example/saikikwok/todolist/AlarmReceiver.java
@@ -4,10 +4,13 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v4.app.NavUtils;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import com.example.saikikwok.todolist.Models.Todo;
 import com.example.saikikwok.todolist.R;
@@ -25,6 +28,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
         Todo todo = intent.getParcelableExtra(TaskActivity.KEY_TASK_DETAILS);
 
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_access_alarm_black_24dp)
                 .setContentTitle(todo.getTask())
@@ -32,9 +36,17 @@ public class AlarmReceiver extends BroadcastReceiver {
                 .setAutoCancel(true);
 
         Intent returnIntent = new Intent(context, TaskActivity.class);
+
         returnIntent.putExtra(TaskActivity.KEY_TASK_DETAILS, todo);
 
-        PendingIntent returnPendingIntent = PendingIntent.getActivity(context, 0, returnIntent, PendingIntent.FLAG_ONE_SHOT);
+        //PendingIntent returnPendingIntent = PendingIntent.getActivity(context, 0, returnIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(returnIntent);
+
+        PendingIntent returnPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_ONE_SHOT);
+
         builder.setContentIntent(returnPendingIntent);
 
         NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
